6a17c0e86e396384a59997d09e79f48f4cc0e6b7
Juan de Dios Marcos Gomez <marg.jd@gmail.com>
2023-11-23 23:39:08 -0600

    avance conprengentions

---
+++ 01-PYTHON-Comprehensions-funciones-errors/04_list.py
@@ -0,0 +1,9 @@
+numbers = []
+for element in range(1,11):
+    numbers.append(element*2)
+
+print(numbers)
+
+#  con list conprenhentions
+numbers_v2 = [element for element in range (1,11) ]
+print(numbers_v2)
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import this
+#import this
+
+# countries = {"MX", "COL", "ARG", "USA"}
+# northAm = {"USA", "CANADA"}
+# centralAm = {"MX", "GT", "BZ"}
+# southAm = {"COL", "BZ", "ARG"}
+
+# new_set = set()
+# new_set= ((countries.union(northAm)).union(centralAm)).union(southAm)
+
+# print(new_set)
